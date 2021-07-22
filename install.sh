597aa228fdcfd14db9ec929b8f8259fcb153cf50
linux <linux@penguin>
2021-07-22 14:05:48 +0700

    x

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc tern-project latexmkrc"    # list of files/folders to symlink in homedir
+files="bashrc vimrc tern-project latexmkrc emacs"    # list of files/folders to symlink in homedir
 
 ##########
 
